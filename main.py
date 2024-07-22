46094b5089926e50e58df101a41ab456e8b83a69
Curtis <cleav31@wgu.edu>
2024-07-22 18:34:43 -0400

    removed Assert

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from omegaconf import DictConfig, OmegaConf
 # This automatically reads in the configuration
 @hydra.main(config_name="config")
 def go(config: DictConfig):
-    
+
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
@@ -20,7 +20,6 @@ def go(config: DictConfig):
         # This was passed on the command line as a comma-separated list of steps
         steps_to_execute = config["main"]["execute_steps"].split(",")
     else:
-        assert isinstance(config["main"]["execute_steps"], list)
         steps_to_execute = config["main"]["execute_steps"]
 
     # Download step
